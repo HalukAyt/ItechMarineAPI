6abb7b6ac65df2e2bbc2240b60372ed200ab11ec
HalukAyt <halukaytis@hotmail.com>
2025-10-28 15:25:21 +0300

    mqtt local change 1.27

--- ItechMarineAPI/Mqtt/MqttOptions.cs
+++ ItechMarineAPI/Mqtt/MqttOptions.cs
@@ -2,7 +2,7 @@
 
 public class MqttOptions
 {
-    public string Host { get; set; } = "192.168.1.40";
+    public string Host { get; set; } = "192.168.1.27";
     public int Port { get; set; } = 1883;
     public bool UseTls { get; set; } = false;
     public string? Username { get; set; }
